58293881b7209d82f19b060b1870f7cd61070099
Sp4rklingTablewater <169612938+Sp4rklingTablewater@users.noreply.github.com>
2026-08-15 13:42:22 +0200

    Update items.js

--- items.js
+++ items.js
@@ -24,19 +24,43 @@ export const items = [
     link: "https://www.volleyballdirekt.de/adidas-dvv-trikot-sleeveless-damen-2026-damen-2435724.html"
   },
   {
+    id: "4",
+    name: "Adidas DVV Trikot sleeveless Damen 2026",
+    description: "VEREDELN > Spielername und- Nummer: ALSMEIER + 9",
+    price: "89,45 €",
+    image: "https://cdn.volleyballdirekt.de/full/AD7DVVJM4687-black.jpg",
+    link: "https://www.volleyballdirekt.de/adidas-dvv-trikot-sleeveless-damen-2026-damen-2435724.html"
+  },
+  {
     id: "2",
-    name: "Kochbuch: Italienisch",
-    description: "Ein schönes Kochbuch mit klassischen italienischen Rezepten.",
-    price: "24 €",
-    image: "https://images.unsplash.com/photo-1544025162-d76694265947?w=600",
-    link: ""
+    name: "Schreibtisch Organizer",
+    description: "Blau.",
+    price: "31,99 €",
+    image: "https://m.media-amazon.com/images/G/03/apparel/rcxgs/tile._CB483369910_.gif",
+    link: "https://www.amazon.de/Marbrasse-Schreibtisch-Briefablage-Briefst%C3%A4nder-Schreibtischzubeh%C3%B6r/dp/B0FV7WMBYP?pd_rd_w=oTc9t&content-id=amzn1.sym.bbac26bb-3f7b-44dd-a8a5-c10fcfb1ed60%3Aamzn1.symc.30e3dbb4-8dd8-4bad-b7a1-a45bcdbc49b8&pf_rd_p=bbac26bb-3f7b-44dd-a8a5-c10fcfb1ed60&pf_rd_r=BN28J18AQ89971BG4A3D&pd_rd_wg=yq5R7&pd_rd_r=93981d23-4bb5-4cbd-9831-5a5e47d873b1&pd_rd_i=B0FV7WMBYP&th=1"
   },
   {
     id: "3",
-    name: "Zimmerpflanze",
+    name: "Neopren Kurzhanteln 1–3 kg",
     description: "Etwas Pflegeleichtes für den Schreibtisch, z. B. eine Monstera.",
-    price: "15 €",
-    image: "https://images.unsplash.com/photo-1416879595882-3373a0480b5b?w=600",
-    link: ""
+    price: "34,90 €",
+    image: "https://m.media-amazon.com/images/I/716oXeY-t0L._AC_SL1500_.jpg",
+    link: "https://www.amazon.de/Hantelset-Kurzhanteln-ergonomischer-rollsicher-Krafttraining/dp/B0GGHNMQ58"
+  },
+  {
+    id: "5",
+    name: "Snackbox mit 7 Fächern",
+    description: "Edelstahl.",
+    price: "14,99 €",
+    image: "https://m.media-amazon.com/images/I/71VslUhyjGL._AC_SL1500_.jpg",
+    link: "https://www.amazon.de/gp/product/B0GHNF3TCN?smid=A1HYSHEKJJDM06&th=1"
+  },
+    {
+    id: "6",
+    name: "Apple Watch SE 3 GPS 40 mm Smartwatch",
+    description: "Aluminiumgehäuse.",
+    price: "249 €",
+    image: "https://m.media-amazon.com/images/I/61sNr8bUALL._AC_SL1500_.jpg",
+    link: "https://www.amazon.de/Apple-Aluminiumgeh%C3%A4use-Sportarmband-Schlaftracker-Herzfrequenzmesser/dp/B0FQG55GFH?dib=eyJ2IjoiMSJ9.TSLqwHRZVe4_10k2OZWYYuupNy99hqwm94f167yfAob4V1uDTUNuLrHLC5s34cNNcj24Ua1gkh623AWbqjduDBCRkzHiVx4dIKEN0CErNJtYHtZ1NFjmqk3wNNkcQ02v_QpteMZojje1RHs3LskqGmeQRFPZRnmsPwj367ILlZeNyssJDFO-R3AOtVrem7psyDHto3g6oDzm9hShw1r-dROG6IHn105xTpWlJdKjpcM.NhG25zHhhjVFMP7VTEqZ-2gaLHwEnoSzXciN_EDzgjE&dib_tag=se&keywords=apple%2Bwatch&qid=1786793944&sr=8-11&th=1"
   }
 ];
